8f0f9cdf5b368a72b8092f6fc2e866fc52087309
Nikhil R Podduturi <poddutur@Nikhils-MBP.fritz.box>
2022-08-18 13:13:03 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Taxi service using reinforcement learning',
+    author='Nikhil R Podduturi',
+    license='MIT',
+)
